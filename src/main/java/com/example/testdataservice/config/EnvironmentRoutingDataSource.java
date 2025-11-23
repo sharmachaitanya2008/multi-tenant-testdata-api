0f60aaaebb1f304cea9249f061de0d6d04b1666c
sharmachaitanya2008 <140507782+sharmachaitanya2008@users.noreply.github.com>
2025-11-23 22:59:59 +0530

    V2.0

--- src/main/java/com/example/testdataservice/config/EnvironmentRoutingDataSource.java
+++
@@ -1,5 +0,0 @@
-package com.example.testdataservice.config;
-import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
-public class EnvironmentRoutingDataSource extends AbstractRoutingDataSource {
-    @Override protected Object determineCurrentLookupKey() { return EnvironmentContext.get(); }
-}
--- src/main/java/com/example/testdataservice/repo/TestDataRepository.java
+++
@@ -1,2 +0,0 @@
-package com.example.testdataservice.repo; import com.example.testdataservice.domain.TestDataEntity; import org.springframework.data.jpa.repository.JpaRepository;
-public interface TestDataRepository extends JpaRepository<TestDataEntity, Long> {}
